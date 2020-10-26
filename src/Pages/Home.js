fa2ce5ec9d72e4f8675733c505a81dffd5d44b45
Dmitriy <shalberkind@mail.ru>
2020-10-26 13:18:49 +0300

    Добавлены карточки

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button, Card, CardDeck, Container } from "react-bootstrap";
 import CarouselBox from "../Components/CarouselBox";
 
 class Home extends Component {
@@ -6,6 +7,53 @@ class Home extends Component {
     return (
       <div>
         <CarouselBox />
+        <Container>
+          <h2 className="text-center m-4">Our Team</h2>
+          <CardDeck className="m-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Developers</Card.Title>
+                <Card.Text>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/1595391/pexels-photo-1595391.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Developers</Card.Title>
+                <Card.Text>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/3153198/pexels-photo-3153198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              />
+              <Card.Body>
+                <Card.Title>Developers</Card.Title>
+                <Card.Text>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </Container>
       </div>
     );
   }

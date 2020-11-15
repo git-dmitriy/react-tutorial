56821517bb03a06e2c065624d6f0e50fb459d1ed
Dmitriy <shalberkind@mail.ru>
2020-11-15 15:44:12 +0300

    Использование роутинга react-router-dom

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,8 +8,13 @@ import {
   Button,
 } from "react-bootstrap";
 import logo from "./logo192.png";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link,
+} from "react-router-dom";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Contacts from "../Pages/Contacts";
@@ -18,26 +23,37 @@ import Blog from "../Pages/Blog";
 class Header extends Component {
   render() {
     return (
-      <>
+      <Router>
         <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="/">
-              <img
-                src={logo}
-                height="30"
-                width="30"
-                classsName="d-inline-block align-top"
-                alt="logo"
-              />
-              {" React site"}
-            </Navbar.Brand>
+            <Link to="/">
+              <Navbar.Brand href="/">
+                <img
+                  src={logo}
+                  height="30"
+                  width="30"
+                  className="d-inline-block align-top"
+                  alt="logo"
+                />
+                {" React site"}
+              </Navbar.Brand>
+            </Link>
+
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <Nav.Link href="/contacts">Contacts</Nav.Link>
-                <Nav.Link href="/blog">Blog</Nav.Link>
+                <NavLink to="/" exact className="nav-link">
+                  Home
+                </NavLink>
+                <NavLink to="/about" className="nav-link">
+                  About Us
+                </NavLink>
+                <NavLink to="/contacts" className="nav-link">
+                  Contacts
+                </NavLink>
+                <NavLink to="/blog" className="nav-link">
+                  Blog
+                </NavLink>
               </Nav>
               <Form inline>
                 <FormControl
@@ -50,15 +66,14 @@ class Header extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/contacts" component={Contacts} />
-            <Route exact path="/blog" component={Blog} />
-          </Switch>
-        </Router>
-      </>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path="/blog" component={Blog} />
+        </Switch>
+      </Router>
     );
   }
 }
